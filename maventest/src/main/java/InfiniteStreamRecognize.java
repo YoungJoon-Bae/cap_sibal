3b7934fa3fa7dcf6929ba6e57928fd8b1c362efa
kwon3 <kwon32220@gmail.com>
2020-05-08 01:27:13 +0900

    중간과정 주석 삭제

--- maventest/src/main/java/InfiniteStreamRecognize.java
+++ maventest/src/main/java/InfiniteStreamRecognize.java
@@ -145,20 +145,20 @@ public class InfiniteStreamRecognize {
               SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
               if (result.getIsFinal()) {
             	  System.out.println(alternative.getTranscript());
-                //System.out.print(GREEN);
-                //System.out.print("\033[2K\r");
-                //System.out.printf(
-                //    "%s: %s [confidence: %.2f]\n",
-                //    convertMillisToDate(correctedTime),
-                //    alternative.getTranscript(),
-                //    alternative.getConfidence());
+                System.out.print(GREEN);
+               System.out.print("\033[2K\r");
+                System.out.printf(
+                 "%s: %s [confidence: %.2f]\n",
+                  convertMillisToDate(correctedTime),
+               alternative.getTranscript(),
+                   alternative.getConfidence());
                 isFinalEndTime = resultEndTimeInMS;
                 lastTranscriptWasFinal = true;
               } else {
-                //System.out.print(RED);
-                //System.out.print("\033[2K\r");
-                //System.out.printf(
-                //    "%s: %s", convertMillisToDate(correctedTime), alternative.getTranscript());
+                System.out.print(RED);
+                System.out.print("\033[2K\r");
+               System.out.printf(
+                   "%s: %s", convertMillisToDate(correctedTime), alternative.getTranscript());
                 lastTranscriptWasFinal = false;
               }
             }
